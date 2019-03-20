de2edd25c40bc3ad2a7bb46854f9903b4bea2555
koenuma <-->
2019-03-20 11:46:06 +0900

    setuo project configuration file

---
+++ config/config.go
@@ -0,0 +1,54 @@
+package config
+
+import (
+	"log"
+	"os"
+	"github.com/BurntSushi/toml"
+)
+
+var Conf Config
+
+// from toml file config.
+type Config struct {
+	DB DataBaseConfig `toml:"database"`
+}
+
+type DataBaseConfig struct {
+	Host     string `toml:"host"`
+	Port     int    `toml:"port"`
+	User     string `toml:"user"`
+	Password string `toml:"password"`
+	DbName   string `toml:"dbName"`
+}
+
+// Initialization configuration for project 
+// every environment.
+func InitConfig() {
+	var config Config
+	var configFile string
+	configDir := "config"
+
+	switch os.Getenv("AUTH465_ENV") {
+	case "local":
+		configFile = "config.local.toml"
+	case "development":
+		configFile = "config.development.toml"
+	case "staging":
+		configFile = "config.staging.toml"
+	case "production":
+		configFile = "config.production.toml"
+	default:
+		configFile = "config.local.toml"
+	}
+
+	_, err := toml.DecodeFile(configDir+"/"+configFile, &config)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if config.DB.Password == "" {
+		os.Getenv("AUTH465_DB_PWD")
+	}
+
+	Conf = config
+}
--- server.go
+++ server.go
@@ -1,11 +1,13 @@
 package main
 
 import (
+	"auth465/config"
 	"auth465/router"
 	"github.com/labstack/echo"
 )
 
 func main() {
+	config.InitConfig()
 	e := echo.New()
 	//e.GET("/", func(c echo.Context) error {
 	//	return c.String(http.StatusOK, "Hello, World!")
