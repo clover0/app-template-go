bd7be58fe572099cd675ed3a289c6599404bc76d
koenuma <-->
2019-05-19 16:59:17 +0900

    add users-counts test

--- store/users_test.go
+++ store/users_test.go
@@ -11,11 +11,13 @@ import (
 	"github.com/jmoiron/sqlx"
 )
 
-func TestUserStore_Create(t *testing.T) {
+func TestUserStore(t *testing.T) {
 	config := testutils.SetUpConfig("../config")
 	testutils.SetupDBWithConfig(config)
 
 	t.Run("it creates successfully", testUserStore_CreateSuccess)
+	t.Run("it counts successfully", testUserStore_CountsSuccess)
+
 }
 
 func testUserStore_CreateSuccess(t *testing.T) {
@@ -30,6 +32,42 @@ func testUserStore_CreateSuccess(t *testing.T) {
 		if res == 0 || err != nil {
 			t.Error("failed create user")
 		}
+		return err
+	})
+	if err != nil {
+		t.Error(err)
+	}
+}
+
+func testUserStore_CountsSuccess(t *testing.T) {
+	num := testutils.GenerateRandomNum()
+	userStoreFn := New()
+	user1 := &core.User{
+		Email:    "test" + fmt.Sprint(testutils.GenerateRandomNum()),
+		Password: "test_password" + fmt.Sprint(num),
+	}
+	user2 := &core.User{
+		Email:    "test" + fmt.Sprint(testutils.GenerateRandomNum()),
+		Password: "test_password" + fmt.Sprint(num),
+	}
+	fmt.Println(dbtest.GetCon())
+	err := db.Transact(dbtest.GetCon(), func(tx *sqlx.Tx) error {
+		res, err := userStoreFn(tx).Create(user1)
+		if res == 0 || err != nil {
+			t.Error("failed create user1")
+			return err
+		}
+		res, err = userStoreFn(tx).Create(user2)
+		if res == 0 || err != nil {
+			t.Error("failed create user2")
+			return err
+		}
+		act, err := userStoreFn(tx).Count("password", "test_password"+fmt.Sprint(num))
+		if act != 2 {
+			t.Fail()
+			return err
+		}
+
 		return err
 	})
 	if err != nil {
