368678c98f4f9d3d773f2a22a1ade1f4e4baeaeb
koenuma <-->
2019-05-21 12:43:23 +0900

    add session handler test

---
+++ handler/session_test.go
@@ -0,0 +1,111 @@
+package handler
+
+import (
+	"auth465/core"
+	"auth465/testutils"
+
+	"fmt"
+	"net/http"
+	"net/http/httptest"
+	"strings"
+	"testing"
+
+	"github.com/labstack/echo"
+	"github.com/stretchr/testify/assert"
+)
+
+type sessionServiceMock struct{}
+
+func (service sessionServiceMock) CreateSession(userId uint32) (string, error) {
+	return "xxxxxx", nil
+}
+
+// FindUserByEmail finds user by email and return user with transaction
+func (service sessionServiceMock) FindUserByEmail(email string) (*core.User, error) {
+	user := &core.User{
+		1,
+		"",
+		"",
+		"email",
+		"password",
+	}
+	return user, nil
+}
+
+func (service sessionServiceMock) FindUserById(id uint32) (*core.User, error) {
+	user := &core.User{
+		1,
+		"",
+		"",
+		"email",
+		"password",
+	}
+	return user, nil
+}
+
+func (service sessionServiceMock) ComparePassword(user *core.User, input string) error {
+	return nil
+}
+
+type sessionServiceMock2 struct{}
+
+func (service sessionServiceMock2) CreateSession(userId uint32) (string, error) {
+	return "xxxxxx", nil
+}
+
+func (service sessionServiceMock2) FindUserByEmail(email string) (*core.User, error) {
+	return nil, nil
+}
+
+func (service sessionServiceMock2) FindUserById(id uint32) (*core.User, error) {
+	return nil, nil
+}
+
+func (service sessionServiceMock2) ComparePassword(user *core.User, input string) error {
+	return nil
+}
+
+func TestCreateSessionHandler(t *testing.T) {
+
+	t.Run("it returns 201", testCreateSession_return201)
+	t.Run("it returns 400", testCreateSession_return400)
+
+}
+
+func testCreateSession_return201(t *testing.T) {
+	userJson := fmt.Sprintf(`{"email": "test%s@app465.com","password":"123456"}`, fmt.Sprint(testutils.GenerateRandomNum()))
+
+	e := echo.New()
+	req := httptest.NewRequest(http.MethodPost, "/", strings.NewReader(userJson))
+	req.Header.Set(echo.HeaderContentType, echo.MIMEApplicationJSON)
+	rec := httptest.NewRecorder()
+	c := e.NewContext(req, rec)
+
+	service := &sessionServiceMock{}
+	handler := CreateSessionHandler(service)
+
+	result := "1\n"
+	if assert.NoError(t, handler(c)) {
+		assert.Equal(t, http.StatusCreated, rec.Code)
+		assert.Equal(t, result, rec.Body.String())
+	}
+}
+
+func testCreateSession_return400(t *testing.T) {
+	userJson := fmt.Sprintf(`{"email": "test%s@app465.com","password":"123456"}`, fmt.Sprint(testutils.GenerateRandomNum()))
+
+	e := echo.New()
+	req := httptest.NewRequest(http.MethodPost, "/", strings.NewReader(userJson))
+	req.Header.Set(echo.HeaderContentType, echo.MIMEApplicationJSON)
+	rec := httptest.NewRecorder()
+	c := e.NewContext(req, rec)
+
+	service := &sessionServiceMock2{}
+	handler := CreateSessionHandler(service)
+
+	result := "\"email no match\"\n"
+	if assert.NoError(t, handler(c)) {
+		assert.Equal(t, http.StatusBadRequest, rec.Code)
+		assert.Equal(t, result, rec.Body.String())
+	}
+}
--- handler/user_test.go
+++ handler/user_test.go
@@ -19,14 +19,24 @@ type userServiceMock struct{}
 func (u *userServiceMock) CheckDuplicateEmail(user *core.User) (bool, error) {
 	return true, nil
 }
-
 func (u *userServiceMock) Register(user *core.User) error {
+	return nil
+}
+
+type userServiceMock2 struct{}
+
+func (u *userServiceMock2) CheckDuplicateEmail(user *core.User) (bool, error) {
+	return false, nil
+}
+
+func (u *userServiceMock2) Register(user *core.User) error {
 	return nil
 }
 
 func TestCreateUserHandler(t *testing.T) {
 
-	t.Run("it returns 201 success", TestCreateUserHandler_return201)
+	t.Run("it returns 201", TestCreateUserHandler_return201)
+	t.Run("it returns 400", TestCreateUserHandler_return201)
 
 }
 
@@ -45,6 +55,26 @@ func TestCreateUserHandler_return201(t *testing.T) {
 	result := "1\n"
 	if assert.NoError(t, handler(c)) {
 		assert.Equal(t, http.StatusCreated, rec.Code)
+		assert.Equal(t, result, rec.Body.String())
+	}
+}
+
+func TestCreateUserHandler_return400(t *testing.T) {
+	num := fmt.Sprint(testutils.GenerateRandomNum())
+	userJson := fmt.Sprintf(`{"email": "test%s@app465.com","password":"123456"}`, num)
+
+	e := echo.New()
+	req := httptest.NewRequest(http.MethodPost, "/", strings.NewReader(userJson))
+	req.Header.Set(echo.HeaderContentType, echo.MIMEApplicationJSON)
+	rec := httptest.NewRecorder()
+	c := e.NewContext(req, rec)
+
+	service := &userServiceMock2{}
+	handler := CreateUserHandler(service)
+
+	result := "\"bad request\"\n"
+	if assert.NoError(t, handler(c)) {
+		assert.Equal(t, http.StatusBadRequest, rec.Code)
 		assert.Equal(t, result, rec.Body.String())
 	}
 }
